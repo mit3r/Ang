7dfa02f4d765c19d96f6a4a73aaa0c7299a5aa2d
Franciszek Kaczmarek <franek.kaczmarek45gmail.com>
2024-06-06 15:21:38 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vite + React + React Router + TypeScript + TailwindCSS
+
+Template to create new project using vrrtt.
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: "./",
+  },
+}
+
